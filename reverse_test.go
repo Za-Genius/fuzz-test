1147b8545e7c575a1e64844782aa7c7b6e5c1903
oluwatobiloba1 <rapzter58@gmail.com>
2025-12-17 07:15:46 +0100

    fix: understanding go fuzz in the test package

---
+++ reverse_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc) // Use f.Add to provide a seed corpus
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, revErr := Reverse(orig)
+		doubleRev, doubleRevErr := Reverse(rev)
+		if doubleRev != orig && doubleRevErr == nil {
+			t.Errorf("Before: %q, after: %q, error: %q", orig, doubleRev, doubleRevErr)
+		}
+
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) && revErr == nil {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
+}
